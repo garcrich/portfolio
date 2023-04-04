ae300b050ce5d73c0935358cc8c3277d3a688708
Ricky Garcia <rg3646@outlook.com>
2023-04-03 19:11:46 -0600

    basic font setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.scss';
+import './styles/index.module.scss';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.scss';
 const Header = () => {
   return (
     <section className={styles.container} data-testid="header">
-      <h2 className={styles.title}>Header</h2>
+      <h2 className={styles.title}>Header right here boi</h2>
     </section>
   )
 }
