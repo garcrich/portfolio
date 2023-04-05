eb44608a9921c5fcc0036287fb31843ed92d25b8
Ricky Garcia <rg3646@outlook.com>
2023-04-04 21:58:20 -0600

    nav bar scaffolding

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import logo from '../../assets/images/logo.svg'
 import styles from './Navbar.module.scss';
 
 
 const NavBar = () => {
   return (
-    <nav className={styles.container} data-testid="navbar">
-      <h2 className={styles.title}>Navbar</h2>
+    <nav className={`${styles.container} ${styles.navContainer}`} data-testid="navbar">
+      <ul className={styles.navMenu}>
+        <li className={styles.logo}><img src={logo} alt="logo"/></li>
+        <li>About</li>
+        <li>Work</li>
+        <li>Projects</li>
+        <li>Recommendations</li>
+        <li>Contact</li>
+        <li>Credentials</li>
+      </ul>
     </nav>
   )
 }
