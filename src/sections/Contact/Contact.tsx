73a376bbffb3c3f5df386bea760a827e010fed88
Ricky Garcia <rg3646@outlook.com>
2023-04-05 13:24:57 -0600

    adds contact info

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -1,12 +1,23 @@
+import React from 'react';
 import styles from './Contact.module.scss';
+import { Button } from '@mui/material';
 
-
-const Contact = () => {
+const Contact: React.FC = () => {
   return (
     <section className={styles.container} data-testid="contact">
-      <h2 className={styles.title}>Contact</h2>
+      <h2>Contact</h2>
+      <p>
+        I enjoy connecting with like-minded professionals and exploring exciting projects. If you're considering a collaboration or have an interesting project in mind, let's turn those ideas into reality!
+      </p>
+      <div className={styles.contactInfo}>
+        <div className={styles.email}>
+          <Button variant='contained'>
+            <a href="mailto:your.rg3646aws@gmail.com">Get in Touch</a>
+          </Button>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Contact;
