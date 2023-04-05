ae2c4c57a3d95a19cbb0cead74ba3d53c076c0ab
Ricky Garcia <rg3646@outlook.com>
2023-04-04 23:05:17 -0600

    setting Hero content

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,10 +1,16 @@
+import { Button } from '@mui/material';
 import styles from './Hero.module.scss';
-
+import HeroLogo from './HeroLogo'
 const Header = () => {
   return (
-    <section className={styles.container} data-testid="hero">
-      <h2 className={styles.title}>Header right here boi</h2>
-    </section>
+  <section className={styles.container} data-testid="hero">
+    <HeroLogo />
+    <p>Hey there, I'm</p>
+    <h1 className={styles.name}>Ricky Garcia</h1>
+    <h2 className={styles.title}>Your Salt Lake City-based frontend engineer</h2>
+    <p className={styles.subtitle}>With a passion for creating accessible, high-performing websites and a knack for problem-solving, I thrive on turning innovative ideas into digital reality.</p>
+    <Button variant="contained" className={`${styles.button}`}>View My Work</Button>
+  </section>
   )
 }
 
