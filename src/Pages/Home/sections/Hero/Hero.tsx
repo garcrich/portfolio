f829bb8193696d11cecc7b2a9295e5cb03377f85
Ricky Garcia <rg3646@outlook.com>
2023-04-09 18:54:36 -0600

    fixing breakpoints

--- src/Pages/Home/sections/Hero/Hero.tsx
+++ src/Pages/Home/sections/Hero/Hero.tsx
@@ -9,7 +9,7 @@ const Header:FC = () => {
     <HeroLogo />
     <p>Hey there, I'm</p>
     <h1 className={styles.name}>Ricky Garcia</h1>
-    <h2 className={styles.title}>Your Salt Lake City-based frontend engineer</h2>
+    <h2 className={`${styles.title} mt-sm`}>Your Salt Lake City-based frontend engineer</h2>
     <p className={styles.subtitle}>With a passion for creating accessible, high-performing websites and a knack for problem-solving, I thrive on turning innovative ideas into digital reality.</p>
     <button className="button">View My Work</button>
   </section>
