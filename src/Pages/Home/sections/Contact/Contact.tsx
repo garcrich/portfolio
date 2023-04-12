1de8bad31062ae84095501f930e3f1196a8d31bb
Ricky Garcia <rg3646@outlook.com>
2023-04-12 03:26:48 -0600

    styling adjustments

--- src/Pages/Home/sections/Contact/Contact.tsx
+++ src/Pages/Home/sections/Contact/Contact.tsx
@@ -4,16 +4,18 @@ import styles from './Contact.module.scss';
 const Contact: FC = () => {
   return (
     <section className={`${styles.contactContainer}`} data-testid="contact">
-      <h2>Let's Connect & Create!</h2>
-      <p>
-        I'm a <span className='ft-clr-3 ft-wt-extra-bold'>collaboration enthusiast</span> eager to connect and create remarkable projects. Ready to chat?
-      </p>
-      <p className={``}>
-        <strong><span className=''>Let's make magic happen!</span></strong>
-      </p>
-      <button className='button mt-md'>
-        <a href="mailto:your.email@example.com">Get in Touch</a>
-      </button>
+      <div className={`${styles.contactContent} section-spacing`}>
+        <h2>Let's Connect & Create!</h2>
+        <p>
+          I'm a <span className='ft-clr-3 ft-wt-extra-bold'>collaboration enthusiast</span> eager to connect and create remarkable projects. Ready to chat?
+        </p>
+        <p className={``}>
+          <strong><span className=''>Let's make magic happen!</span></strong>
+        </p>
+        <button className='button mt-md'>
+          <a href="mailto:your.email@example.com">Get in Touch</a>
+        </button>
+      </div>
     </section>
   );
 };
