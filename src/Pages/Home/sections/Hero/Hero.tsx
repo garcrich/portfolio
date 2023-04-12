0962109e565174c2b975ca7ffb5a54ee19504783
Ricky Garcia <rg3646@outlook.com>
2023-04-12 03:03:25 -0600

    clean up

--- src/Pages/Home/sections/Hero/Hero.tsx
+++ src/Pages/Home/sections/Hero/Hero.tsx
@@ -7,8 +7,8 @@ const Header:FC = () => {
   <section className={styles.heroContainer} data-testid="hero">
     <div className={styles.contentContainer}>
       <h3 className={`ft-wt-norm mb-0`}>Hey there, I'm</h3>
-      <h1 className={`${styles.name} mt-0 mb-0 ft-wt-bold`}>Ricky Garcia</h1>
-      <h2 className={`${styles.location} mt-sm mb-0 ft-wt-bold`}>Your very own Salt Lake City-based</h2>
+      <h1 className={`${styles.name} mt-0 mb-0`}>Ricky Garcia</h1>
+      <h2 className={`${styles.location} mt-sm mb-0`}>Your very own Salt Lake City-based</h2>
       <div className={`${styles.titleContainer} mt-0`}>
         <h2 className={`${styles.title} mt-0 mb-0`}><strong className="ft-clr-2">frontend engineer</strong>.</h2>
         <HeroLogo />
