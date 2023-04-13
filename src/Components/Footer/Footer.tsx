b1438c1ab853d4b5ab35b40b025cd7868ca89929
Ricky Garcia <rg3646@outlook.com>
2023-04-12 20:17:47 -0600

    builds out footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,12 +1,20 @@
 import styles from './Footer.module.scss';
-
+import { ImLinkedin } from "react-icons/im";
+import { FaGithub } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
     <footer className={styles.footer} data-testid="footer">
-      <p>Linkedin</p>
-      <p>GitHub</p>
-      <p>Email</p>
+      <a href="https://www.linkedin.com/in/ricky-g-garcia/" target="_blank" rel="noopener noreferrer">
+        <ImLinkedin />
+      </a>
+      <a href="https://github.com/garcrich" target="_blank" rel="noopener noreferrer">
+        <FaGithub />
+      </a>
+      <a href="mailto:your.rg3646aws@gmail.com">
+        <MdEmail />
+      </a>
     </footer>
   )
 }
--- src/Pages/ErrorPage/ErrorPage.tsx
+++ src/Pages/ErrorPage/ErrorPage.tsx
@@ -2,15 +2,21 @@
 import { FC } from 'react';
 import styles from './ErrorPage.module.scss'
 import { Link } from "react-router-dom";
+import NavBar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
 
 
 const ErrorPage:FC = () => {
   return (
-  <section className={styles.errorPageContainer} data-testid="error-page">
-    <h1>Ooops! That's illegal!</h1>
-    <iframe title="illegal" src="https://giphy.com/embed/f8lDluiWJ7yQTtdS3L" width="480" height="270" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
-    <button className={`button ${styles.errorBtn} mt-md`}><Link to={`/`}>Return to home</Link></button>
-  </section>
+    <>
+      <NavBar />
+      <section className={styles.errorPageContainer} data-testid="error-page">
+        <h1>Ooops! That's illegal!</h1>
+        <iframe title="illegal" src="https://giphy.com/embed/f8lDluiWJ7yQTtdS3L" width="480" height="270" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+        <button className={`button ${styles.errorBtn} mt-md`}><Link to={`/`}>Return to home</Link></button>
+      </section>
+      <Footer />
+    </>
   )
 }
 
