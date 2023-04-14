3278a80166f64e874c8df70d05d9f4e73fbef2bb
Ricky Garcia <rg3646@outlook.com>
2023-04-14 17:25:59 -0600

    fixing build errors

--- src/App/Pages/Home/sections/Hero/useReactSpring.ts
+++ src/App/Pages/Home/sections/Hero/useReactSpring.ts
@@ -1,7 +1,7 @@
 import { useSpring } from '@react-spring/web'
 
 const useCustomSpring = (delay: number,factor = 1) => {
-  const [spring, api] = useSpring(() => ({
+  const [spring] = useSpring(() => ({
     from: { opacity: 0, transform: 'translateY(10px)' },
     to: { opacity: 1, transform: 'translateY(0px)' },
     delay: delay * factor,
