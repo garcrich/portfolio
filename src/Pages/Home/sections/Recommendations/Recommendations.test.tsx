c675cd6691b3a8d9be6b20dc1a5c5178bdc18f13
Ricky Garcia <rg3646@outlook.com>
2023-04-14 02:04:24 -0600

    Fix unit test

--- src/Pages/Home/sections/Recommendations/Recommendations.test.tsx
+++ src/Pages/Home/sections/Recommendations/Recommendations.test.tsx
@@ -23,10 +23,18 @@ describe('slideBuilder', () => {
     expect(result).toBeInstanceOf(Array);
     expect(result[0]).toBeInstanceOf(Array);
   });
-  it('returns an array of arrays of the correct length', () => {
+  it('returns an array of arrays of the correct length on desktop', async() => {
+    window.innerWidth =  await 767;
     const result = slideBuilder(2, recommendationsData);
-    expect(result.length).toBe(2);
+    expect(result.length).toBe(3);
     expect(result[0].length).toBe(2);
     expect(result[1].length).toBe(2);
+  });
+  it('returns an array of arrays of the correct length on mobile', async() => {
+    window.innerWidth =  await 766;
+    const result = slideBuilder(1, recommendationsData);
+    expect(result.length).toBe(6);
+    expect(result[0].length).toBe(1);
+    expect(result[1].length).toBe(1);
   });
 });
